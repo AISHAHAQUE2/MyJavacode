726242a35cb1532cf32926625c083227bf82fffd
AISHAHAQUE2 <92319665+AISHAHAQUE2@users.noreply.github.com>
2021-10-12 00:18:06 +0530

    Add files via upload

---
+++ permutation.java
@@ -0,0 +1,27 @@
+﻿
+   import java.util.*;  
+    public class Main 
+    {  
+      public static void main(String[] args)  
+        {  
+            int n, r, per, fact1, fact2;  
+            Scanner sc =  new Scanner(System.in);  
+            System.out.println("Enter the Value of n and r?");  
+            n = sc.nextInt();  
+            r = sc.nextInt();  
+            fact1 = n;  
+            for (int i = n - 1; i >= 1; i--)  
+            {  
+                fact1 = fact1 * i;  
+            }  
+            int number;  
+            number = n - r;  
+            fact2 = number;  
+            for (int i = number - 1; i >= 1; i--)  
+            {  
+                fact2 = fact2 * i;  
+            }  
+            per = fact1 / fact2;  
+            System.out.println("nPr = "+per);  
+    }  
+}  
